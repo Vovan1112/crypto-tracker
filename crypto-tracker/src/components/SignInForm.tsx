6b283457317ca4fec797ec76754531b34bf30215
Volodymyr Babinchuk <volodymyrbabinchuk@coherentsolutions.com>
2023-10-24 16:05:53 +0300

    added_contact-page

--- crypto-tracker/src/components/SignInForm.tsx
+++ crypto-tracker/src/components/SignInForm.tsx
@@ -26,7 +26,7 @@ const SignInForm = () => {
     }
 
     return (
-        <form className="py-6 flex flex-col sm:py-12" onSubmit={handleSubmit}>
+        <form className=" py-6 flex flex-col sm:py-12" onSubmit={handleSubmit}>
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
             <div
                 className="absolute inset-0 bg-gradient-to-r from-blue-500 to-custom-main shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
